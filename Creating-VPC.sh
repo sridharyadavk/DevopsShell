c6bf04075345a7dd7373617e1bcbe37b94618aaa
sridharyadavk <nani10557@gmail.com>
2022-08-23 23:53:46 +0530

    added ouput type as json and get the details in jq table format

--- Creating-VPC.sh
+++ Creating-VPC.sh
@@ -4,4 +4,4 @@ set -x
 A=1
 B=2
 echo "Lets Get The VPC Details From The Region $REGION"
-aws ec2 describe-vpcs --region $REGION --output jso 
+aws ec2 describe-vpcs --region $REGION --output json | jq ".Vpcs[].VpcId"
