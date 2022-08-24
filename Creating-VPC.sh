6220ef28aacd585a62e55896cbbded883b4083ac
sridharyadavk <nani10557@gmail.com>
2022-08-24 15:07:42 +0530

    Removed unwanted arguments

--- Creating-VPC.sh
+++ Creating-VPC.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 REGION=$1
 set -x -e
-echo "1"
-echo "2"
-echo "3"
-A=1
-B=2
 echo "Lets Get The VPC Details From The Region $REGION"
 aws ec2 describe-vpcs --region $REGION --output json | jq ".Vpcs[].VpcId"
