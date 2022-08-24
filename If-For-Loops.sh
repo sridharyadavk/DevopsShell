e889266e76c17ba671a94bfdd2afc2248fffae87
sridharyadavk <nani10557@gmail.com>
2022-08-24 20:21:04 +0530

    made corrections

--- If-For-Loops.sh
+++ If-For-Loops.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REGION=('us-east-1'  'us-east-2' 'ind-hyd-1' 'ap-south-1' 'eu-west-1')
+REGIONS=('us-east-1'  'us-east-2' 'ind-hyd-1' 'ap-south-1' 'eu-west-1')
 for REGION in ${REGIONS[@]}; do
    echo "The Region Name is  $REGION"
    echo "------------------------------------------------------------------"
